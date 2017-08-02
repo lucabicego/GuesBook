d42e1158325033a72d1d157fa337cffa5417d0bb
lucabicego <luca639.bicego@gmail.com>
2017-08-02 11:55:57 +0200

    Sistemato Errore Visualizzazione Body

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var bodyParser = require("body-parser");
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 3000;   // Port 3000 if you run locally 
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally 
 var app = express();
-app.set('port',port);
-app.set('address',address);
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 var entries = [];
@@ -29,7 +27,7 @@ app.post("/new-entry", function(request, response)
         response.status(400).send("Entries must have a title and a body.");
         return;
      }
-     entries.push({title: request.body.title,content: request.body.body,published: new Date()});
+     entries.push({title: request.body.title,body: request.body.body,published: new Date()});
      response.redirect("/");
   });
 app.use(function(request, response) 
@@ -37,10 +35,19 @@ app.use(function(request, response)
 	  response.status(404).render("404");
   });  
 var listener = app.listen(port, address, function(){
-     console.log("Guestbook app started on address "+app.get('address'));
-     console.log("Guestbook app started on port "+app.get('port'));
+     console.log("Guestbook app started on address "+address);
+     console.log("Guestbook app started on port "+port);
 });  
 /*
+Nota:
+   Se si usa http.createServer(...) in OpenShift si ottiene il seguente errore:
+   
+   events.js:182
+      throw er; // Unhandled 'error' event
+      ^
+   Error: listen EADDRINUSE :::8080
+   
+
 http.createServer(app).listen(port, function() 
   {
      console.log("Guestbook app started on address "+app.get('address'));
